ed917505d10255f49f0568ae58cbc9a63008c8c6
Guilherme Lodi <guilherme.tlodi@gmail.com>
2020-03-29 18:43:06 -0300

    Refactoring duplicates methods

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -3,10 +3,33 @@ package guru.springframework;
 public class Money {
 
     protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount, "CHF");
+    }
+
+    protected String currency() {
+        return currency;
+    }
+
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
 
-        return amount == money.amount;
+        return amount == money.amount
+                && currency == money.currency;
     }
 }
